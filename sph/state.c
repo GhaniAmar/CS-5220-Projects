bf8f543ef2804ed87e7f0a58d684d3514d283dab
Sujay Jayakar <sujayakar314@gmail.com>
2011-10-30 02:21:01 -0400

    Started on linked list implementation, but without structures. After rereading the notes, I feel I should give that another shot (quickly).

--- sph/state.c
+++ sph/state.c
@@ -1,16 +1,37 @@
 #include <stdlib.h>
 #include "state.h"
+#include "math.h"
+
 
 sim_state_t* alloc_state(int n)
 {
     sim_state_t* s = (sim_state_t*) calloc(1, sizeof(sim_state_t));
-    s->n   =  n;
-    s->rho =  (float*) calloc(  n, sizeof(float));
-    s->x =    (float*) calloc(2*n, sizeof(float));
-    s->vh =   (float*) calloc(2*n, sizeof(float));
-    s->v =    (float*) calloc(2*n, sizeof(float));
-    s->a =    (float*) calloc(2*n, sizeof(float));
+    s->n     = n;
+    s->binw  = (int) floor(sqrt(n));
+    s->nbins = s->binw * s->binw;
+    s->rho   = (float*) calloc(  n, sizeof(float));
+    s->x     = (float*) calloc(2*n, sizeof(float));
+    s->vh    = (float*) calloc(2*n, sizeof(float));
+    s->v     = (float*) calloc(2*n, sizeof(float));
+    s->a     = (float*) calloc(2*n, sizeof(float));
+    s->bins  = (node**) calloc(s->nbins, sizeof(node*));
+
     return s;
+}
+
+void clear_bins(sim_state_t* s) {
+    int i;
+    node *curr, *temp;
+
+    for (i = 0; i < s -> nbins; ++i) {
+        curr = s->bins[i];
+
+        while (curr != NULL) {
+            temp =  curr->next;
+            free(curr);
+            curr = temp;
+        }
+    }
 }
 
 void free_state(sim_state_t* s)
@@ -20,5 +41,9 @@ void free_state(sim_state_t* s)
     free(s->vh);
     free(s->x);
     free(s->rho);
+
+    clear_bins(s);
+    free(s->bins);
+
     free(s);
 }
--- sph/state.h
+++ sph/state.h
@@ -3,25 +3,34 @@
 
 /*@T
  * \section{System state}
- * 
+ *
  * The [[sim_state_t]] structure holds the information for the current
  * state of the system and of the integration algorithm.  The array
  * [[x]] has length $2n$, with [[ x[2*i+0] ]] and [[ x[2*i+1] ]] representing
  * the $x$ and $y$ coordinates of the particle positions.  The layout
  * for [[v]], [[vh]], and [[a]] is similar, while [[rho]] only has one
  * entry per particle.
- * 
+ *
  * The [[alloc_state]] and [[free_state]] functions take care of storage
  * for the local simulation state.
  *@c*/
+
+typedef struct node {
+    int index;
+    struct node* next;
+} node;
+
 typedef struct sim_state_t {
-    int n;                /* Number of particles    */
-    float mass;           /* Particle mass          */
-    float* restrict rho;  /* Densities              */
-    float* restrict x;    /* Positions              */
-    float* restrict vh;   /* Velocities (half step) */
-    float* restrict v;    /* Velocities (full step) */
-    float* restrict a;    /* Acceleration           */
+    int n;                 /* Number of particles                              */
+    int binw;              /* Number of bin columns (i.e. nbins = binw * binw) */
+    int nbins;             /* Number of particle bins                          */
+    float mass;            /* Particle mass                                    */
+    float* restrict rho;   /* Densities                                        */
+    float* restrict x;     /* Positions                                        */
+    float* restrict vh;    /* Velocities (half step)                           */
+    float* restrict v;     /* Velocities (full step)                           */
+    float* restrict a;     /* Acceleration                                     */
+    node** restrict bins;  /* Particle bin array                               */
 } sim_state_t;
 
 sim_state_t* alloc_state(int n);
