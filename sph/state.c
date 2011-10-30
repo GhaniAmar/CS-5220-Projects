400811479845e529871a912e6b484c9da65c4c1a
Sujay Jayakar <sujayakar314@gmail.com>
2011-10-30 02:43:30 -0400

    Added new particle structures in state.c/h (in addition to previous code for debugging help).

--- sph/state.c
+++ sph/state.c
@@ -1,15 +1,25 @@
 #include <stdlib.h>
 #include "state.h"
+#include "math.h"
+
+#include <stdio.h>
 
 sim_state_t* alloc_state(int n)
 {
     sim_state_t* s = (sim_state_t*) calloc(1, sizeof(sim_state_t));
-    s->n   =  n;
-    s->rho =  (float*) calloc(  n, sizeof(float));
-    s->x =    (float*) calloc(2*n, sizeof(float));
-    s->vh =   (float*) calloc(2*n, sizeof(float));
-    s->v =    (float*) calloc(2*n, sizeof(float));
-    s->a =    (float*) calloc(2*n, sizeof(float));
+    s->n           = n;
+    s->rho         = (float*) calloc(  n, sizeof(float));
+    s->x           = (float*) calloc(2*n, sizeof(float));
+    s->vh          = (float*) calloc(2*n, sizeof(float));
+    s->v           = (float*) calloc(2*n, sizeof(float));
+    s->a           = (float*) calloc(2*n, sizeof(float));
+
+    s->nbins       = (int) floor(sqrt(n));
+    s->particles   = (particle_t*) calloc(s->n, sizeof(particle_t));
+    s->bins        = (particle_t**) calloc(s->nbins, sizeof(particle_t*));
+
+    printf("Created %d by %d particle bins.\n", s->nbins, s->nbins);
+
     return s;
 }
 
@@ -20,5 +30,9 @@ void free_state(sim_state_t* s)
     free(s->vh);
     free(s->x);
     free(s->rho);
+
+    free(s->particles);
+    free(s->bins);
+
     free(s);
 }
--- sph/state.h
+++ sph/state.h
@@ -3,25 +3,40 @@
 
 /*@T
  * \section{System state}
- * 
+ *
  * The [[sim_state_t]] structure holds the information for the current
  * state of the system and of the integration algorithm.  The array
  * [[x]] has length $2n$, with [[ x[2*i+0] ]] and [[ x[2*i+1] ]] representing
  * the $x$ and $y$ coordinates of the particle positions.  The layout
  * for [[v]], [[vh]], and [[a]] is similar, while [[rho]] only has one
  * entry per particle.
- * 
+ *
  * The [[alloc_state]] and [[free_state]] functions take care of storage
  * for the local simulation state.
  *@c*/
+
+typedef struct particle_t {
+    float rho;                  /* Density                */
+    float x[2];                 /* Positions              */
+    float vh[2];                /* Velocities (half step) */
+    float v[2];                 /* Velocities (full step) */
+    float a[2];                 /* Acceleration           */
+    struct particle_t *next;    /* Next particle in bin   */
+} particle_t;
+
 typedef struct sim_state_t {
-    int n;                /* Number of particles    */
-    float mass;           /* Particle mass          */
-    float* restrict rho;  /* Densities              */
-    float* restrict x;    /* Positions              */
-    float* restrict vh;   /* Velocities (half step) */
-    float* restrict v;    /* Velocities (full step) */
-    float* restrict a;    /* Acceleration           */
+    int n;                 /* Number of particles        */
+    float mass;            /* Particle mass              */
+    float* restrict rho;   /* Densities                  */
+    float* restrict x;     /* Positions                  */
+    float* restrict vh;    /* Velocities (half step)     */
+    float* restrict v;     /* Velocities (full step)     */
+    float* restrict a;     /* Acceleration               */
+
+    int nbins;
+    particle_t* particles; /* Array of particles         */
+    particle_t** bins;     /* Array of bin head pointers */
+
 } sim_state_t;
 
 sim_state_t* alloc_state(int n);
