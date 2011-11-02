2f96d7f2762a8f4b44e72a740e73a346699ce949
Sujay Jayakar <sujayakar314@gmail.com>
2011-11-01 20:41:01 -0400

    Revert "Added todo to readme and added const decorations."
    This reverts commit fbbd97ba49718e57620536fc55bfa0e259ed2902.
    
    Conflicts:
    
    	README
    	sph/interact.c

--- sph/interact.c
+++ sph/interact.c
@@ -142,7 +142,7 @@ void compute_accel(sim_state_t* state, sim_param_t* params)
     int i, pass;
 
     // Unpack system state
-    const int n = state->n;
+    int n = state->n;
 
     // Compute density and color
     compute_density(state, params);
@@ -156,56 +156,58 @@ void compute_accel(sim_state_t* state, sim_param_t* params)
     }
 
     // Constants for interaction term
-    const float C0 = mass / M_PI / ( (h2)*(h2) );
-    const float Cp =  15*k;
-    const float Cv = -40*mu;
-    pass = 0;
+    float C0 = mass / M_PI / ( (h2)*(h2) );
+    float Cp =  15*k;
+    float Cv = -40*mu;
 
-    #pragma omp parallel shared(state,pass)
-    {
-        while (pass < 3) {
+    for (pass = 0; pass < 3; ++pass) {
 
-            #pragma omp for schedule(static)
-            for (i = pass; i < nbinwidth; i += 3) {
-                float x, y, dx, dy, r2, rhoi, rhoj, q, u, w0, wp, wv, dvx, dvy, fx, fy;
-                particle_t *node_buffer[4];
-                particle_t *curr, *curr_bin;
-                int j, l, mbins;
+        /* Used the same trick as compute_density here for parallelization */
+        /* We make three passes over the bins, doing every third bin in    */
+        /* parallel. This prevents any blocking between the threads, as    */
+        /* cannot access the same particle within a pass.                  */
+        #pragma omp parallel for shared(state)
+        for (i = pass; i < nbinwidth; i += 3) {
+            float x, y, dx, dy, r2, rhoi, rhoj, q, u, w0, wp, wv, dvx, dvy, fx, fy;
+            particle_t *node_buffer[4];
+            particle_t *curr, *curr_bin;
+            int j, l, mbins;
 
-                for (j = 0; j < nbinwidth; ++j) {
-                    curr_bin = state->bins[i * nbinwidth + j];
+            for (j = 0; j < nbinwidth; ++j) {
+                curr_bin = state->bins[i * nbinwidth + j];
 
-                    while (curr_bin) {
-                        x = curr_bin->x[0];
-                        y = curr_bin->x[1];
-                        rhoi = curr_bin->rho;
-                        get_neighboring_bins(state, curr_bin, node_buffer, &mbins);
+                while (curr_bin) {
+                    assert(!curr_bin->flag);
 
-                        for (l = 0; l < mbins; ++l) {
-                            curr = node_buffer[l];
+                    x = curr_bin->x[0];
+                    y = curr_bin->x[1];
+                    rhoi = curr_bin->rho;
+                    get_neighboring_bins(state, curr_bin, node_buffer, &mbins);
 
-                            while(curr) {
-                                if (!curr->flag) {
-                                    dx = x - curr->x[0];
-                                    dy = y - curr->x[1];
-                                    r2 = dx*dx + dy*dy;
+                    for (l = 0; l < mbins; ++l) {
+                        curr = node_buffer[l];
 
-                                    if (r2 > 0 && r2 < h2) {
-                                        rhoj = curr->rho;
-                                        q = sqrt(r2)/h;
-                                        u = 1 - q;
-                                        w0 = C0 * u/rhoi/rhoj;
-                                        wp = w0 * Cp * (rhoi+rhoj-2*rho0) * u/q;
-                                        wv = w0 * Cv;
-                                        dvx = (state->particles[i].v[0]) - (curr -> v[0]);
-                                        dvy = (state->particles[i].v[1]) - (curr -> v[1]);
-                                        fx = wp*dx + wv*dvx;
-                                        fy = wp*dy + wv*dvy;
-                                        curr_bin->a[0] += fx;
-                                        curr_bin->a[1] += fy;
-                                        curr->a[0]     -= fx;
-                                        curr->a[1]     -= fy;
-                                    }
+                        while(curr) {
+                            if (!curr->flag) {
+                                dx = x - curr->x[0];
+                                dy = y - curr->x[1];
+                                r2 = dx*dx + dy*dy;
+
+                                if (r2 > 0 && r2 < h2) {
+                                    rhoj = curr->rho;
+                                    q = sqrt(r2)/h;
+                                    u = 1 - q;
+                                    w0 = C0 * u/rhoi/rhoj;
+                                    wp = w0 * Cp * (rhoi+rhoj-2*rho0) * u/q;
+                                    wv = w0 * Cv;
+                                    dvx = (state->particles[i].v[0]) - (curr -> v[0]);
+                                    dvy = (state->particles[i].v[1]) - (curr -> v[1]);
+                                    fx = wp*dx + wv*dvx;
+                                    fy = wp*dy + wv*dvy;
+                                    curr_bin->a[0] += fx;
+                                    curr_bin->a[1] += fy;
+                                    curr->a[0]     -= fx;
+                                    curr->a[1]     -= fy;
                                 }
 
                                 curr = curr->next;
