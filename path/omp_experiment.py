09d83781711d5e76ba39a1f4b4afacd2f4eb6449
Sujay Jayakar <sujayakar314@gmail.com>
2011-11-06 21:19:11 -0500

    Added options to vary the threads in the experiment script.

--- path/omp_experiment.py
+++ path/omp_experiment.py
@@ -3,23 +3,22 @@ import subprocess
 import os
 import sys
 
-def node_experiment(nodes, results):
-    threads = 8
-    qsub = 'run_omp_%d.qsub' % nodes
+def node_experiment(nodes, threads, results):
+    qsub = 'run_omp_%s_%d.qsub' % (threads, nodes)
 
-    make_qsub('path-omp.x -n %d' % nodes, str(threads),
+    make_qsub('path-omp.x -n %d' % nodes, threads,
               '%s/%d.out' % (results, nodes), qsub)
     os.system('qsub %s' % qsub)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print 'Arguments please: start stop step'
+    if len(sys.argv) < 5:
+        print 'Arguments please: start stop step threads'
     else:
-        results = 'omp_exp'
-        app, start, stop, step = sys.argv
+        _, start, stop, step, threads = sys.argv
+        results = 'exp_omp_' + threads
         os.system('rm -r -f %s' % results)
         os.system('mkdir %s' % results)
         os.system('make')
 
         for nodes in range(int(start), int(stop), int(step)):
-            node_experiment(nodes, results)
+            node_experiment(nodes, threads, results)
